96c424d301cf1dcca377bcb7df792414f2291d73
StankovicDusan49 <dusanstankovicbg@gmail.com>
2021-12-29 11:49:24 +0100

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.Label();
+            this.artikal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_artikal = new System.Windows.Forms.TextBox();
+            this.txt_cena = new System.Windows.Forms.TextBox();
+            this.txt_kom = new System.Windows.Forms.TextBox();
+            this.txt_popust = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Location = new System.Drawing.Point(120, 65);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(15, 13);
+            this.id.TabIndex = 0;
+            this.id.Text = "id";
+            // 
+            // artikal
+            // 
+            this.artikal.AutoSize = true;
+            this.artikal.Location = new System.Drawing.Point(120, 101);
+            this.artikal.Name = "artikal";
+            this.artikal.Size = new System.Drawing.Size(35, 13);
+            this.artikal.TabIndex = 1;
+            this.artikal.Text = "artikal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "kom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(120, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "cena";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(120, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "popust";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(206, 62);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(158, 20);
+            this.txt_id.TabIndex = 5;
+            // 
+            // txt_artikal
+            // 
+            this.txt_artikal.Location = new System.Drawing.Point(206, 98);
+            this.txt_artikal.Name = "txt_artikal";
+            this.txt_artikal.Size = new System.Drawing.Size(158, 20);
+            this.txt_artikal.TabIndex = 6;
+            // 
+            // txt_cena
+            // 
+            this.txt_cena.Location = new System.Drawing.Point(206, 168);
+            this.txt_cena.Name = "txt_cena";
+            this.txt_cena.Size = new System.Drawing.Size(158, 20);
+            this.txt_cena.TabIndex = 7;
+            // 
+            // txt_kom
+            // 
+            this.txt_kom.Location = new System.Drawing.Point(206, 138);
+            this.txt_kom.Name = "txt_kom";
+            this.txt_kom.Size = new System.Drawing.Size(158, 20);
+            this.txt_kom.TabIndex = 8;
+            // 
+            // txt_popust
+            // 
+            this.txt_popust.Location = new System.Drawing.Point(206, 197);
+            this.txt_popust.Name = "txt_popust";
+            this.txt_popust.Size = new System.Drawing.Size(158, 20);
+            this.txt_popust.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(401, 265);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(496, 265);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = ">>";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(206, 342);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "obrisi";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(323, 342);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "promeni";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(450, 342);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "dodaj";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_popust);
+            this.Controls.Add(this.txt_kom);
+            this.Controls.Add(this.txt_cena);
+            this.Controls.Add(this.txt_artikal);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.artikal);
+            this.Controls.Add(this.id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label artikal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_artikal;
+        private System.Windows.Forms.TextBox txt_cena;
+        private System.Windows.Forms.TextBox txt_kom;
+        private System.Windows.Forms.TextBox txt_popust;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string cs = "Data source=DESKTOP-CQJ6GR7; initial catalog=Prodavnica; integrated security=true";
+        public int red = 0;
+        DataTable stavka = new DataTable();
+        public void osvezi(int x)
+        {
+            txt_id.Text = stavka.Rows[x]["ID"].ToString();
+            txt_artikal.Text = stavka.Rows[x]["artikal"].ToString();
+            txt_kom.Text = stavka.Rows[x]["kom"].ToString();
+            txt_cena.Text = stavka.Rows[x]["cena"].ToString();
+            txt_popust.Text = stavka.Rows[x]["popust"].ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_id.Enabled = false;
+            SqlConnection veza = new SqlConnection(cs);
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from stavka", veza);
+            adapter.Fill(stavka);
+            osvezi(red);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            red = 0;
+            osvezi(red);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (red != 0)
+            {
+                red--;
+                osvezi(red);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (red != stavka.Rows.Count - 1)
+            {
+                red++;
+                osvezi(red);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            red = stavka.Rows.Count - 1;
+            osvezi(red);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand("Delete from stavka where id=" + txt_id.Text, veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from stavka", veza);
+            stavka.Clear();
+            adapter.Fill(stavka);
+            if (red == stavka.Rows.Count)
+            {
+                red--;
+            }
+            osvezi(red);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand("Update stavka set artikal='" + txt_artikal.Text + "' ,kom='" + txt_kom.Text + "',cena='" + txt_cena.Text + "',popust='" + txt_popust.Text + "' where id=" + txt_id.Text, veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from stavka", veza);
+            stavka.Clear();
+            adapter.Fill(stavka);
+            osvezi(red);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand("Insert into stavka (artikal,kom,cena,popust) values ('" + txt_artikal.Text + "' ,'" + txt_kom.Text + "' ,'" + txt_cena.Text + "' ,'" + txt_popust.Text + "')", veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from stavka", veza);
+            stavka.Clear();
+            adapter.Fill(stavka);
+            osvezi(red);
+        }
+    }
+}
